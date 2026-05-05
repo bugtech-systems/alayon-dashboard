bd8a65bde7b39747549e7f12fe00a8c9dcb04579
I am root <bugtechsystems@gmail.com>
2026-05-05 10:44:11 +0800

    Fix things"

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -34,7 +34,20 @@ const nextConfig = {
       },
     ],
   },
-  allowedDevOrigins: ['192.168.1.140', 'localhost', '127.0.0.1'],
+  allowedDevOrigins: ['192.168.1.140', 'localhost', '127.0.0.1', 'sharewin.pro', 'alayon.store'],
+   async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+      },
+    ];
+  }
 }
 
 export default nextConfig
