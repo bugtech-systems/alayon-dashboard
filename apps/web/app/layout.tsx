9da3679177dc4b748ae6a6a68bde7a95388a125e
I am root <bugtechsystems@gmail.com>
2026-07-24 23:38:21 +0800

    Fix things

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -52,7 +52,7 @@ export default async function RootLayout({
         <MedusaAuthProvider>
         <Providers>
             <AuthProvider>
- 
+  
           {children}
           </AuthProvider>
         </Providers>
